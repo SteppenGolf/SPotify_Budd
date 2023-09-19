0f78f07ffefff4b2f4e87d28f46a4f05b458be63
SteppenGolf <113161452+SteppenGolf@users.noreply.github.com>
2023-09-19 14:55:31 -0500

    Add files via upload

---
+++ Spotfiy_Client.h
@@ -0,0 +1,85 @@
+#pragma once
+#include<Arduino.h>
+#include<WiFiClientSecure.h>
+#include"ArduinoJson.h"
+
+struct Http_Result{
+
+    int http_Code;
+    String payload;
+
+};
+
+
+struct song_Details{
+    uint32_t duration_Ms;
+    String album;
+    String artist;
+    String song;
+    String Id;
+};
+
+class Spotify_Client {
+   
+    private:
+
+    WiFiClientSecure client;
+    String client_Id;
+    String client_Secret;
+    String redirect_Uri;
+    String access_Token;
+    String refresh_Token;
+    String device_Id;
+    String device_Name;
+
+    Http_Result CallAPI(String method, String url, String body);
+    
+    String GetDeviceId(String json);
+
+    // root certificate for spotify.com 
+    const char* digicert_root_ca= \
+    "-----BEGIN CERTIFICATE-----\n" \
+    "MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh\n" \
+    "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
+    "d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n" \
+    "QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT\n" \
+    "MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n" \
+    "b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG\n" \
+    "9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB\n" \
+    "CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97\n" \
+    "nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt\n" \
+    "43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P\n" \
+    "T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4\n" \
+    "gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO\n" \
+    "BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR\n" \
+    "TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw\n" \
+    "DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr\n" \
+    "hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg\n" \
+    "06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF\n" \
+    "PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls\n" \
+    "YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk\n" \
+    "CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=\n" \
+    "-----END CERTIFICATE-----\n";
+
+
+    public:
+
+    song_Details currentSong;
+    float currSongPositionMs;
+    float lastSongPositionMs;
+    int currVol;
+  
+    Spotify_Client(String client_Id, String client_Secret,  String device_Name, String refresh_Token );
+    song_Details get_track_Info();
+    void fetch_Token();
+    int toggle_Play(String context_uri);
+    int vol_Adjust(int vol);
+    int skip_Back();
+    int Shuffle();
+    int skip_Forward();
+    Http_Result CallAPI( String method, String url, String body );
+    String get_Devices();
+
+};
+
+
